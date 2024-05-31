cf2bb5108e475e5d484624a80f6f26e8234e93b5
Joanna Hulek <joanna.hulek@gmail.com>
2024-05-31 15:30:18 +0200

    h2 styling updated, cards added to copywriting component, SVG icons added to Contact

--- src/components/Copywriting.tsx
+++ src/components/Copywriting.tsx
@@ -2,7 +2,47 @@ export default function Copywriting() {
     return (
         <>
             <h2>Copywriting</h2>
-            Tu będą kafelki
+
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                            <a href="#" className="card-link">Card link</a>
+                            <a href="#" className="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a short card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural
+                                lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
