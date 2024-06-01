6e87f7bf47850e76ab0bf4abab24bc786a1c3a0e
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-01 02:39:36 +0200

    Idę spać, bo już wymyślam głupoty ;-)

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,8 +1,8 @@
 export default function Alert(){
     return (
         <>
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
+            <button type="button" className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Warning
             </button>
 
             <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -13,13 +13,18 @@ export default function Alert(){
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            Proszę przeglądać w kasku ochronnym BHP!
+                            <div className="alert alert-warning" role="alert">
+                                Proszę przeglądać w kasku ochronnym BHP!
+                            </div>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">OK, rozumiem!</button>
+                            <button type="button" className="btn btn-outline-danger" data-bs-dismiss="modal">OK, rozumiem!</button>
                         </div>
                     </div>
                 </div>
-            </div></>
+            </div>
+
+
+        </>
     )
 }
