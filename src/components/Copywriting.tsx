24b6a609b5329983345c2183cb9e760b9a2262e3
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-01 02:07:55 +0200

    Oferta

--- src/components/Copywriting.tsx
+++ src/components/Copywriting.tsx
@@ -7,19 +7,19 @@ export default function Copywriting() {
                 <div className="col">
                     <div className="card h-100">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                            <h5 className="card-title text-uppercase">Copywriting</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary"></h6>
                             <p className="card-text">Some quick example text to build on the card title and make up the
                                 bulk of the card's content.</p>
-                            <a href="#" className="card-link">Card link</a>
-                            <a href="#" className="card-link">Another link</a>
+                            <a href="#portfolio" className="card-link">Portfolio</a>
+                            <a href="#contact" className="card-link">Zamów</a>
                         </div>
                     </div>
                 </div>
                 <div className="col">
                     <div className="card h-100">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title text-uppercase">Social Media</h5>
                             <p className="card-text">This is a short card.</p>
                         </div>
                     </div>
@@ -27,7 +27,7 @@ export default function Copywriting() {
                 <div className="col">
                     <div className="card h-100">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title text-uppercase">Pozycjonowanie</h5>
                             <p className="card-text">This is a longer card with supporting text below as a natural
                                 lead-in to additional content.</p>
                         </div>
@@ -36,7 +36,16 @@ export default function Copywriting() {
                 <div className="col">
                     <div className="card h-100">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title text-uppercase">UX/UI</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title text-uppercase">Grafika</h5>
                             <p className="card-text">This is a longer card with supporting text below as a natural
                                 lead-in to additional content. This content is a little bit longer.</p>
                         </div>
