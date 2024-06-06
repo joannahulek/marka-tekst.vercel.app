1b38aacc2e50a6a83cc9ce1c75473de09aa739c8
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-06 22:16:39 +0200

    porfolio - rendering in progress

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,50 +1,87 @@
+import { ReactElement } from "react";
+
+export type SampleItem = {
+    id: number
+    label: string;
+    collapsed: boolean;
+    description: ReactElement
+}
+
+const prodSample1: SampleItem ={
+    id: 0,
+    label: "Przykładowy opis produktu",
+    collapsed: true,
+    description:(<>
+        <h4>Maxi Cosi CabrioFix i-size</h4>
+        <p>
+            Maxi Cosi CabrioFix i-size to fotelik początkowy, dedykowany dla noworodków i niemowląt o wadze do 12 kg i
+            wzroście od 40 cm do 75 cm. Fotelik montowany tyłem do kierunku jazdy przy pomocy wygodnej i stabilnej bazy
+            ISOFIX lub przy pomocy samochodowych pasów bezpieczeństwa. CabrioFix i-size to komfort i bezpieczeństwo
+            Twojego dziecka.
+        </p>
+        <p>
+            Maxi Cosi to marka pochodząca z Holandii, która specjalizuje się w produkcji samochodowych fotelików
+            dziecięcych oraz wózków. Producent dba o bezpieczeństwo dzieci nieprzerwanie od 1984 r., stale ulepszając
+            swoje produkty, aby każda podróż była bezpieczna i komfortowa.
+        </p>
+        <h4>Maxi Cosi CabrioFix i-sizeMaxi Cosi CabrioFix i-Size -- inwestycja w bezpieczeństwo Twojego dziecka</h4>
+        <p>
+            Fotelik RWF Maxi Cosi CabrioFix i-Size to model należący do klasy wagowej 0-12 kg, tj. 0+. Jest on wyposażony w funkcjonalną wkładkę niemowlęcą, pozwalającą na bezpieczne przewożenie noworodków.
+        </p>
+        <p>
+            Norma i-Size, to funkcjonujący od 2013 r. system homologacji, oceniający bezpieczeństwo dziecka przewożonego w foteliku. Spełnienie restrykcyjnych, ujednoliconych wymagań sprawia, że produkt uznawany jest za bezpieczny, na dowód czego uzyskuje specjalny certyfikat. Modele fotelików z certyfikatem i-Size sprawdzane są m.in. pod kątem bezpieczeństwa Malucha w czasie kolizji bocznych. Dodatkowo jeszcze lepiej chronią głowę i szyję dziecka.
+        </p>
+    </>)
+
+}
+
+const samples: SampleItem[] = [prodSample1, prodSample1]
+
 export default function Portfolio() {
-return (
-    <>
+    return (
+        <>
         <h2>
             Przykładowy opis akordeonu
         </h2>
 
         <div className="accordion" id="accordionExample">
-            <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Accordion Item #1
-                    </button>
-                </h2>
-                <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the
-                        collapse
-                        plugin adds the appropriate classes that we use to style each element. These classes control the
-                        overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of
-                        this with custom CSS or overriding our default variables. It's also worth noting that just about
-                        any
-                        HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                        <br></br>
-                        <a href="#" target="_blank">Link</a>
+            {samples.map((sample) => (
+                <div className="accordion-item">
+                    <div className="accordion-header">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <h3>{sample.label}</h3>
+                        </button>
+                    </div>
+                    <div id="collapseOne" className="accordion-collapse collapse show"
+                         data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {sample.description}
+                        </div>
                     </div>
                 </div>
-            </div>
+            ))}
             <div className="accordion-item">
                 <h2 className="accordion-header">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Accordion Item #2
+                            data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Accordion Item #3
                     </button>
                 </h2>
-                <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
+                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
+                        collapse plugin adds the appropriate classes that we use to style each element. These
+                        classes
+                        control the overall appearance, as well as the showing and hiding via CSS transitions. You
+                        can
+                        modify any of this with custom CSS or overriding our default variables. It's also worth
+                        noting
                         that
-                        just about any HTML can go within the <code>.accordion-body</code>, though the transition does
+                        just about any HTML can go within the <code>.accordion-body</code>, though the transition
+                        does
                         limit
                         overflow.
-                        <a href="#" target="_blank">Link</a>
                     </div>
                 </div>
             </div>
@@ -58,11 +95,15 @@ return (
                 <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
                         <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
+                        collapse plugin adds the appropriate classes that we use to style each element. These
+                        classes
+                        control the overall appearance, as well as the showing and hiding via CSS transitions. You
+                        can
+                        modify any of this with custom CSS or overriding our default variables. It's also worth
+                        noting
                         that
-                        just about any HTML can go within the <code>.accordion-body</code>, though the transition does
+                        just about any HTML can go within the <code>.accordion-body</code>, though the transition
+                        does
                         limit
                         overflow.
                     </div>
@@ -70,66 +111,7 @@ return (
             </div>
         </div>
 
-        <br></br>
 
-        <h2>Przykładowy opis produktu</h2>
-
-        <div className="card h-100">
-            <div className="row">
-                <div className="col-4">
-                    <nav id="navbar-example3" className="h-100 flex-column align-items-stretch pe-4 border-end">
-                        <nav className="nav nav-pills flex-column">
-                            <a className="nav-link" href="#item-1">Item 1</a>
-                            <nav className="nav nav-pills flex-column">
-                                <a className="nav-link ms-3 my-1" href="#item-1-1">Item 1-1</a>
-                                <a className="nav-link ms-3 my-1" href="#item-1-2">Item 1-2</a>
-                            </nav>
-                            <a className="nav-link" href="#item-2">Item 2</a>
-                            <a className="nav-link" href="#item-3">Item 3</a>
-                            <nav className="nav nav-pills flex-column">
-                                <a className="nav-link ms-3 my-1" href="#item-3-1">Item 3-1</a>
-                                <a className="nav-link ms-3 my-1" href="#item-3-2">Item 3-2</a>
-                            </nav>
-                        </nav>
-                    </nav>
-                </div>
-
-                <div className="col-8">
-                    <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-smooth-scroll="true"
-                         className="scrollspy-example-2" tabIndex={-1}>
-                        <div id="item-1">
-                            <h4>Item 1</h4>
-                            <p>...</p>
-                        </div>
-                        <div id="item-1-1">
-                            <h5>Item 1-1</h5>
-                            <p>...</p>
-                        </div>
-                        <div id="item-1-2">
-                            <h5>Item 1-2</h5>
-                            <p>...</p>
-                        </div>
-                        <div id="item-2">
-                            <h4>Item 2</h4>
-                            <p>...</p>
-                        </div>
-                        <div id="item-3">
-                            <h4>Item 3</h4>
-                            <p>...</p>
-                        </div>
-                        <div id="item-3-1">
-                            <h5>Item 3-1</h5>
-                            <p>...</p>
-                        </div>
-                        <div id="item-3-2">
-                            <h5>Item 3-2</h5>
-                            <p>...</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </>
+</>
 )
 }
