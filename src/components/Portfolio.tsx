f5992df3db5e6ce883a5183592d09b3c88c9bd70
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-08 18:57:36 +0200

    porfolio - content extracted

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,117 +1,51 @@
-import { ReactElement } from "react";
-
-export type SampleItem = {
-    id: number
-    label: string;
-    collapsed: boolean;
-    description: ReactElement
-}
-
-const prodSample1: SampleItem ={
-    id: 0,
-    label: "Przykładowy opis produktu",
-    collapsed: true,
-    description:(<>
-        <h4>Maxi Cosi CabrioFix i-size</h4>
-        <p>
-            Maxi Cosi CabrioFix i-size to fotelik początkowy, dedykowany dla noworodków i niemowląt o wadze do 12 kg i
-            wzroście od 40 cm do 75 cm. Fotelik montowany tyłem do kierunku jazdy przy pomocy wygodnej i stabilnej bazy
-            ISOFIX lub przy pomocy samochodowych pasów bezpieczeństwa. CabrioFix i-size to komfort i bezpieczeństwo
-            Twojego dziecka.
-        </p>
-        <p>
-            Maxi Cosi to marka pochodząca z Holandii, która specjalizuje się w produkcji samochodowych fotelików
-            dziecięcych oraz wózków. Producent dba o bezpieczeństwo dzieci nieprzerwanie od 1984 r., stale ulepszając
-            swoje produkty, aby każda podróż była bezpieczna i komfortowa.
-        </p>
-        <h4>Maxi Cosi CabrioFix i-sizeMaxi Cosi CabrioFix i-Size -- inwestycja w bezpieczeństwo Twojego dziecka</h4>
-        <p>
-            Fotelik RWF Maxi Cosi CabrioFix i-Size to model należący do klasy wagowej 0-12 kg, tj. 0+. Jest on wyposażony w funkcjonalną wkładkę niemowlęcą, pozwalającą na bezpieczne przewożenie noworodków.
-        </p>
-        <p>
-            Norma i-Size, to funkcjonujący od 2013 r. system homologacji, oceniający bezpieczeństwo dziecka przewożonego w foteliku. Spełnienie restrykcyjnych, ujednoliconych wymagań sprawia, że produkt uznawany jest za bezpieczny, na dowód czego uzyskuje specjalny certyfikat. Modele fotelików z certyfikatem i-Size sprawdzane są m.in. pod kątem bezpieczeństwa Malucha w czasie kolizji bocznych. Dodatkowo jeszcze lepiej chronią głowę i szyję dziecka.
-        </p>
-    </>)
-
-}
-
-const samples: SampleItem[] = [prodSample1, prodSample1]
+import getSamples, {SampleItem} from "./content/PortfolioContent";
 
 export default function Portfolio() {
+    const samples = getSamples()
     return (
         <>
-        <h2>
-            Przykładowy opis akordeonu
-        </h2>
-
-        <div className="accordion" id="accordionExample">
-            {samples.map((sample) => (
-                <div className="accordion-item">
-                    <div className="accordion-header">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            <h3>{sample.label}</h3>
-                        </button>
-                    </div>
-                    <div id="collapseOne" className="accordion-collapse collapse show"
-                         data-bs-parent="#accordionExample">
-                        <div className="accordion-body">
-                            {sample.description}
-                        </div>
-                    </div>
-                </div>
-            ))}
-            <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Accordion Item #3
-                    </button>
-                </h2>
-                <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These
-                        classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You
-                        can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth
-                        noting
-                        that
-                        just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does
-                        limit
-                        overflow.
-                    </div>
-                </div>
+            <div className="accordion" id="accordionExample">
+                {samples.map((sample) => (
+                    <>
+                        {sample.collapsed ? collapsedSample(sample) : nonCollapsedSample(sample)}
+                    </>
+                ))}
             </div>
-            <div className="accordion-item">
-                <h2 className="accordion-header">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Accordion Item #3
-                    </button>
-                </h2>
-                <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These
-                        classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You
-                        can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth
-                        noting
-                        that
-                        just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does
-                        limit
-                        overflow.
-                    </div>
-                </div>
+        </>
+    )
+}
+
+function nonCollapsedSample(sample: SampleItem) {
+    return <div className="accordion-item">
+        <div className="accordion-header">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse"
+                    data-bs-target={"#collapse" + sample.id} aria-expanded="true"
+                    aria-controls={"collapse" + sample.id}>
+                <h3>{sample.label}</h3>
+            </button>
+        </div>
+        <div id={"collapse"+sample.id} className="accordion-collapse collapse show"
+             data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+                {sample.description}
             </div>
         </div>
-
-
-</>
-)
+    </div>;
 }
+
+function collapsedSample(sample: SampleItem) {
+    return <div className="accordion-item">
+        <div className="accordion-header">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target={"#collapse"+sample.id} aria-expanded="false" aria-controls={"collapse"+sample.id}>
+                <h3>{sample.label}</h3>
+            </button>
+        </div>
+        <div id={"collapse"+sample.id} className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+                {sample.description}
+            </div>
+        </div>
+    </div>;
+}
+
---
+++ src/components/content/PortfolioContent.tsx
@@ -0,0 +1,116 @@
+import { ReactElement } from "react";
+
+export type SampleItem = {
+    id: number
+    label: string;
+    collapsed: boolean;
+    description: ReactElement
+}
+
+export default function getSamples(){
+    return [prodSample1, prodSample2, prodSample3]}
+
+const prodSample1: SampleItem ={
+    id: 0,
+    label: "Przykładowy opis produktu",
+    collapsed: false,
+    description:(<>
+        <h4>Maxi Cosi CabrioFix i-size</h4>
+        <p>
+            Maxi Cosi CabrioFix i-size to fotelik początkowy, dedykowany dla noworodków i niemowląt o wadze do 12 kg i
+            wzroście od 40 cm do 75 cm. Fotelik montowany tyłem do kierunku jazdy przy pomocy wygodnej i stabilnej bazy
+            ISOFIX lub przy pomocy samochodowych pasów bezpieczeństwa. CabrioFix i-size to komfort i bezpieczeństwo
+            Twojego dziecka.
+        </p>
+        <p>
+            Maxi Cosi to marka pochodząca z Holandii, która specjalizuje się w produkcji samochodowych fotelików
+            dziecięcych oraz wózków. Producent dba o bezpieczeństwo dzieci nieprzerwanie od 1984 r., stale ulepszając
+            swoje produkty, aby każda podróż była bezpieczna i komfortowa.
+        </p>
+        <h4>Maxi Cosi CabrioFix i-sizeMaxi Cosi CabrioFix i-Size -- inwestycja w bezpieczeństwo Twojego dziecka</h4>
+        <p>
+            Fotelik RWF Maxi Cosi CabrioFix i-Size to model należący do klasy wagowej 0-12 kg, tj. 0+. Jest on
+            wyposażony w funkcjonalną wkładkę niemowlęcą, pozwalającą na bezpieczne przewożenie noworodków.
+        </p>
+        <p>
+            Norma i-Size, to funkcjonujący od 2013 r. system homologacji, oceniający bezpieczeństwo dziecka przewożonego
+            w foteliku. Spełnienie restrykcyjnych, ujednoliconych wymagań sprawia, że produkt uznawany jest za
+            bezpieczny, na dowód czego uzyskuje specjalny certyfikat. Modele fotelików z certyfikatem i-Size sprawdzane
+            są m.in. pod kątem bezpieczeństwa Malucha w czasie kolizji bocznych. Dodatkowo jeszcze lepiej chronią głowę
+            i szyję dziecka.
+        </p>
+        <h4>Maxi Cosi CabrioFix i-Size - zalety</h4>
+        <p>
+            Model CabrioFix i-Size jest fotelikiem cenionym przez rodziców. Pozwala na wygodne i komfortowe podróżowanie
+            z Maluchem już od pierwszych dni życia. Wśród najważniejszych zalet wymienia się:
+            <ul>
+                <li>
+                    lekkość i funkcjonalność - CabrioFix i-Size cechuje wyjątkowo niska waga fotelika samochodowego
+                    (zaledwie 3,2 kg), co ułatwia przenoszenie dziecka, wyjmowanie i wkładanie fotelika do samochodu;
+                </li>
+                <li>
+                    łatwość użytkowania i montażu - model Maxi Cosi CabrioFix i-Size jest kompatybilny z większością
+                    dostępnych obecnie stelaży (również innych producentów);
+                </li>
+                <li>
+                    dwustopniowa regulacja uprzęży.
+                </li>
+            </ul>
+        </p>
+        <p>
+            Dbając o komfort dziecka i rodziców, producent wyposażył fotelik CabrioFix i-Size w wygodne gadżety, takie
+            jak daszek z filtrem UV, pozwalający częściowo osłonić Malucha przed słońcem lub deszczem. Dużą zaletą jest
+            też łatwo zmywalna tapicerka, dzięki której szybko i sprawnie zadbać można o czystość fotelika.
+        </p>
+        <p>
+            Zależy Ci na poprawie mobilności? Elastyczny system podróży oraz możliwość łatwej instalacji nosidła na
+            stelażu wózka to wygodna alternatywa dla Ciebie i Twojego Malucha.
+        </p>
+        <h4>Jak zamontować fotelik Maxi Cosi CabrioFix i-Size?</h4>
+        <p>
+            Model CabrioFix i-Size przystosowany jest do montażu tyłem do kierunku ruchu, zgodnie z wymogami obowiązującymi dla grupy wagowej 0+. W zależności od potrzeb oraz rodzaju auta, może być instalowany przy pomocy pasów bezpieczeństwa lub specjalnej bazy ISOFIX (o ile w samochodzie jest możliwość jej instalacji, w przeciwnym razie konieczne może być zastosowanie specjalnych adapterów).
+        </p>
+        <p>
+            Szukasz odpowiedniej, kompatybilnej bazy? Skontaktuj się z nami.
+        </p>
+    </>)
+}
+
+const prodSample2: SampleItem ={
+    id: 1,
+    label: "Przykładowy wpis na bloga",
+    collapsed: true,
+    description:(<>
+        <h4>Najlepszy fotelik dla noworodka - czym wyróżnia się Maxi Cosi CabrioFix i-Size?</h4>
+        <p>
+            Świadomość dotycząca bezpieczeństwa dzieci w samochodzie wśród rodziców wzrasta z roku na rok. Dzięki temu szukają coraz lepszych, bardziej bezpiecznych, a jednocześnie wygodnych dla dziecka modeli. Jednym z przykładów jest Maxi Cosi CabrioFix i-Size. Co sprawia, że jest on polecany przez specjalistów i ceniony przez rodziców?
+            Ogólna charakterystyka fotelika samochodowego Maxi Cosi CabrioFix i-Size
+            Fotelik samochodowy Maxi Cosi CabrioFix i-Size to model przygotowany dla najmłodszych dzieci, w klasie wagowej do 12 kg oraz o wzroście od 40 cm do maksymalnie 75 cm. Jest to fotelik typu RWF, czyli montowany tyłem do kierunku jazdy. Model ten może być zainstalowany w pojeździe przy pomocy pasów bezpieczeństwa lub kompatybilnej bazy ISOFIX.
+            Fotelik samochodowy wyposażony jest w uprząż 3-punktową, natomiast pasy można wygodnie regulować (dostępne są dwa poziomy regulacji). Dodatkowym atutem jest wkładka noworodkowa, dzięki której maluch może podróżować bezpiecznie, komfortowo i wygodnie już od pierwszych chwil życia. W zestawie z fotelikiem dostępna jest funkcjonalna, doczepiana budka, pozwalająca osłonić małego człowieka przed promieniami słońca lub niewielkim deszczem w czasie przenoszenia.
+            Skorupa fotelika, w porównaniu do poprzednich wersji oferowanych przez producenta, została dość istotnie przebudowana, jednak nadal cechuje się ona prostą budową, dzięki czemu całość jest bardzo lekka. Waga fotelika to zaledwie 3,2 kg, czyli tyle, ile waży statystyczny noworodek.
+            Bezpieczeństwo dziecka w czasie wypadku, czyli jak oceniany jest CabrioFix i-Size w testach zderzeniowych?
+            Testy zderzeniowe (crashtest) oraz coraz bardziej znany i popularny ADAC, to jeden z kluczowych czynników, jaki brany jest pod uwagę przez rodziców, poszukujących fotelika dla swojej pociechy.
+            Podczas testowania, foteliki samochodowe dla dzieci przechodzą przez szereg bardzo restrykcyjnych testów. Dzięki nim producent, a finalnie również rodzice, uzyskują informacje na temat bezpieczeństwa dziecka w danym modelu. Co do zasady - im wyższa ocena, tym fotelik jest bezpieczniejszy. Wyniki podawane są w 5 stopniowej skali, zupełnie jak w szkole. Warto dodać, że w specyfikacji niemieckiej jest odwrotnie - im niższa ocena, tym lepiej.
+            Maxi Cosi CabrioFix i-Size wypada bardzo dobrze. Ogólna ocena bezpieczeństwa fotelika wynosi 5, co jest najwyższą możliwą oceną. W crashtestach brano pod uwagę m.in. stabilność mocowania fotelika w samochodzie podczas wypadku, zarówno w czasie zderzenia czołowego, jak i bocznego. Dodatkowo, w testach ADAC ocenie podlegają również inne czynniki związane z użytkowaniem fotelika, takie jak łatwość montażu i obsługi, ergonomię oraz jakość wykorzystanych materiałów. Ważne są zatem wszystkie kwestie, począwszy od bezpieczeństwa, przez wygodę malucha, aż po komfort korzystania (montażu) dla rodziców.
+            Wygoda i komfort dziecka w czasie podróży
+            Mimo że testy zderzeniowe są najbardziej miarodajne, bo warunki, w jakich są testowane foteliki dla dzieci są naprawdę trudne (zupełnie jak w przypadku prawdziwej kolizji), to dużą uwagę należy zwrócić także na wygodę dziecka. Bardzo ważną kwestią jest ergonomia fotelika, szczególnie w przypadku fotelików dla noworodków i niemowląt do 1 roku życia. Dzieci po urodzeniu nie potrafią jeszcze samodzielnie utrzymać głowy, ich kręgosłup i mięśnie są na tyle słabe, że maluch nie potrafi utrzymać odpowiedniej pozycji ciała. Właściwa ergonomia fotelika i niemowlęcej wkładki pozwalają na wygodną i komfortową podróż, zapewniając odpowiednie wsparcie małemu człowiekowi. To dzięki ergonomii zarówno głowa, jak i tułów czy miednica są ułożone prawidłowo. Dlaczego jest to tak ważne? Ponieważ nieprawidłowości na tym etapie mogą mieć wpływ na całe życie dziecka Dodatkowo, u noworodków, odpowiednie ułożenie ciała zapewnia maluchowi drożność dróg oddechowych.
+
+            Maxi Cosi CabrioFix i-Size to bezpieczny, bardzo dobrej jakości fotelik, który zapewni dziecku bezpieczeństwo i wygodną jazdę. Dużym atutem jest możliwość montażu zarówno przy pomocy pasów samochodowych, jak również kompatybilnej bazy ISOFIX. Dodatkową zaletą jest możliwość umieszczenia fotelika na dopasowanym stelażu wózka.
+            Jeśli masz pytania dotyczące fotelika lub szukasz specjalistów, którzy pomogą Ci prawidłowo zamontować fotelik dla Twojego dziecka - skontaktuj się z nami.
+
+        </p>
+    </>)
+}
+
+const prodSample3: SampleItem ={
+    id: 3,
+    label: "Więcej tekstów",
+    collapsed: true,
+    description:(<a href="https://www.medicover.pl/o-zdrowiu/superbakteria-new-delhi-w-polsce-jak-uniknac-zakazenia,6317,n,3707" target="_blank">
+        <h4>Superbakteria New Delhi w Polsce - jak uniknąć zakażenia?</h4>
+        <p>
+            Artykuł dla portalu medicover.pl
+        </p>
+    </a>)
+}
+
