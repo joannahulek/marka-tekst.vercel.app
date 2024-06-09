02608711723927972c3c955f3a5ad76200890892
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-09 19:42:11 +0200

    PortfolioContent updated

--- src/components/content/PortfolioContent.tsx
+++ src/components/content/PortfolioContent.tsx
@@ -8,7 +8,7 @@ export type SampleItem = {
 }
 
 export default function getSamples(){
-    return [prodSample1, prodSample2, prodSample3]}
+    return [prodSample1, blogSample1, prodSample3]}
 
 const prodSample1: SampleItem ={
     id: 0,
@@ -76,41 +76,87 @@ const prodSample1: SampleItem ={
     </>)
 }
 
-const prodSample2: SampleItem ={
+const blogSample1: SampleItem ={
     id: 1,
     label: "Przykładowy wpis na bloga",
     collapsed: true,
     description:(<>
-        <h4>Najlepszy fotelik dla noworodka - czym wyróżnia się Maxi Cosi CabrioFix i-Size?</h4>
+        <h2>Najlepszy fotelik dla noworodka - czym wyróżnia się Maxi Cosi CabrioFix i-Size?</h2>
         <p>
-            Świadomość dotycząca bezpieczeństwa dzieci w samochodzie wśród rodziców wzrasta z roku na rok. Dzięki temu szukają coraz lepszych, bardziej bezpiecznych, a jednocześnie wygodnych dla dziecka modeli. Jednym z przykładów jest Maxi Cosi CabrioFix i-Size. Co sprawia, że jest on polecany przez specjalistów i ceniony przez rodziców?
-            Ogólna charakterystyka fotelika samochodowego Maxi Cosi CabrioFix i-Size
-            Fotelik samochodowy Maxi Cosi CabrioFix i-Size to model przygotowany dla najmłodszych dzieci, w klasie wagowej do 12 kg oraz o wzroście od 40 cm do maksymalnie 75 cm. Jest to fotelik typu RWF, czyli montowany tyłem do kierunku jazdy. Model ten może być zainstalowany w pojeździe przy pomocy pasów bezpieczeństwa lub kompatybilnej bazy ISOFIX.
-            Fotelik samochodowy wyposażony jest w uprząż 3-punktową, natomiast pasy można wygodnie regulować (dostępne są dwa poziomy regulacji). Dodatkowym atutem jest wkładka noworodkowa, dzięki której maluch może podróżować bezpiecznie, komfortowo i wygodnie już od pierwszych chwil życia. W zestawie z fotelikiem dostępna jest funkcjonalna, doczepiana budka, pozwalająca osłonić małego człowieka przed promieniami słońca lub niewielkim deszczem w czasie przenoszenia.
-            Skorupa fotelika, w porównaniu do poprzednich wersji oferowanych przez producenta, została dość istotnie przebudowana, jednak nadal cechuje się ona prostą budową, dzięki czemu całość jest bardzo lekka. Waga fotelika to zaledwie 3,2 kg, czyli tyle, ile waży statystyczny noworodek.
-            Bezpieczeństwo dziecka w czasie wypadku, czyli jak oceniany jest CabrioFix i-Size w testach zderzeniowych?
-            Testy zderzeniowe (crashtest) oraz coraz bardziej znany i popularny ADAC, to jeden z kluczowych czynników, jaki brany jest pod uwagę przez rodziców, poszukujących fotelika dla swojej pociechy.
-            Podczas testowania, foteliki samochodowe dla dzieci przechodzą przez szereg bardzo restrykcyjnych testów. Dzięki nim producent, a finalnie również rodzice, uzyskują informacje na temat bezpieczeństwa dziecka w danym modelu. Co do zasady - im wyższa ocena, tym fotelik jest bezpieczniejszy. Wyniki podawane są w 5 stopniowej skali, zupełnie jak w szkole. Warto dodać, że w specyfikacji niemieckiej jest odwrotnie - im niższa ocena, tym lepiej.
-            Maxi Cosi CabrioFix i-Size wypada bardzo dobrze. Ogólna ocena bezpieczeństwa fotelika wynosi 5, co jest najwyższą możliwą oceną. W crashtestach brano pod uwagę m.in. stabilność mocowania fotelika w samochodzie podczas wypadku, zarówno w czasie zderzenia czołowego, jak i bocznego. Dodatkowo, w testach ADAC ocenie podlegają również inne czynniki związane z użytkowaniem fotelika, takie jak łatwość montażu i obsługi, ergonomię oraz jakość wykorzystanych materiałów. Ważne są zatem wszystkie kwestie, począwszy od bezpieczeństwa, przez wygodę malucha, aż po komfort korzystania (montażu) dla rodziców.
-            Wygoda i komfort dziecka w czasie podróży
-            Mimo że testy zderzeniowe są najbardziej miarodajne, bo warunki, w jakich są testowane foteliki dla dzieci są naprawdę trudne (zupełnie jak w przypadku prawdziwej kolizji), to dużą uwagę należy zwrócić także na wygodę dziecka. Bardzo ważną kwestią jest ergonomia fotelika, szczególnie w przypadku fotelików dla noworodków i niemowląt do 1 roku życia. Dzieci po urodzeniu nie potrafią jeszcze samodzielnie utrzymać głowy, ich kręgosłup i mięśnie są na tyle słabe, że maluch nie potrafi utrzymać odpowiedniej pozycji ciała. Właściwa ergonomia fotelika i niemowlęcej wkładki pozwalają na wygodną i komfortową podróż, zapewniając odpowiednie wsparcie małemu człowiekowi. To dzięki ergonomii zarówno głowa, jak i tułów czy miednica są ułożone prawidłowo. Dlaczego jest to tak ważne? Ponieważ nieprawidłowości na tym etapie mogą mieć wpływ na całe życie dziecka Dodatkowo, u noworodków, odpowiednie ułożenie ciała zapewnia maluchowi drożność dróg oddechowych.
-
-            Maxi Cosi CabrioFix i-Size to bezpieczny, bardzo dobrej jakości fotelik, który zapewni dziecku bezpieczeństwo i wygodną jazdę. Dużym atutem jest możliwość montażu zarówno przy pomocy pasów samochodowych, jak również kompatybilnej bazy ISOFIX. Dodatkową zaletą jest możliwość umieszczenia fotelika na dopasowanym stelażu wózka.
-            Jeśli masz pytania dotyczące fotelika lub szukasz specjalistów, którzy pomogą Ci prawidłowo zamontować fotelik dla Twojego dziecka - skontaktuj się z nami.
-
+            Świadomość dotycząca bezpieczeństwa dzieci w samochodzie wśród rodziców wzrasta z roku na rok. Dzięki temu
+            szukają coraz lepszych, bardziej bezpiecznych, a jednocześnie wygodnych dla dziecka modeli. Jednym z
+            przykładów jest Maxi Cosi CabrioFix i-Size. Co sprawia, że jest on polecany przez specjalistów i ceniony
+            przez rodziców?
         </p>
+
+        <h4>Najlepszy fotelik dla noworodka - czym wyróżnia się Maxi Cosi CabrioFix i-Size?Ogólna charakterystyka
+            fotelika samochodowego Maxi Cosi CabrioFix i-Size</h4>
+        <p>
+            Fotelik samochodowy Maxi Cosi CabrioFix i-Size to model przygotowany dla najmłodszych dzieci, w klasie
+            wagowej do 12 kg oraz o wzroście od 40 cm do maksymalnie 75 cm. Jest to fotelik typu RWF, czyli montowany
+            tyłem do kierunku jazdy. Model ten może być zainstalowany w pojeździe przy pomocy pasów bezpieczeństwa lub
+            kompatybilnej bazy ISOFIX.
+        </p>
+        <p>
+            Fotelik samochodowy wyposażony jest w uprząż 3-punktową, natomiast pasy można wygodnie regulować (dostępne
+            są dwa poziomy regulacji). Dodatkowym atutem jest wkładka noworodkowa, dzięki której maluch może podróżować
+            bezpiecznie, komfortowo i wygodnie już od pierwszych chwil życia. W zestawie z fotelikiem dostępna jest
+            funkcjonalna, doczepiana budka, pozwalająca osłonić małego człowieka przed promieniami słońca lub niewielkim
+            deszczem w czasie przenoszenia.
+        </p>
+        <p>
+            Skorupa fotelika, w porównaniu do poprzednich wersji oferowanych przez producenta, została dość istotnie
+            przebudowana, jednak nadal cechuje się ona prostą budową, dzięki czemu całość jest bardzo lekka. Waga
+            fotelika to zaledwie 3,2 kg, czyli tyle, ile waży statystyczny noworodek.
+        </p>
+
+        <h4>Bezpieczeństwo dziecka w czasie wypadku, czyli jak oceniany jest CabrioFix i-Size w testach
+            zderzeniowych?</h4>
+        <p>
+            Testy zderzeniowe (crashtest) oraz coraz bardziej znany i popularny ADAC, to jeden z kluczowych czynników,
+            jaki brany jest pod uwagę przez rodziców, poszukujących fotelika dla swojej pociechy.
+        </p>
+        <p>
+            Podczas testowania, foteliki samochodowe dla dzieci przechodzą przez szereg bardzo restrykcyjnych testów.
+            Dzięki nim producent, a finalnie również rodzice, uzyskują informacje na temat bezpieczeństwa dziecka w
+            danym modelu. Co do zasady - im wyższa ocena, tym fotelik jest bezpieczniejszy. Wyniki podawane są w 5
+            stopniowej skali, zupełnie jak w szkole. Warto dodać, że w specyfikacji niemieckiej jest odwrotnie - im
+            niższa ocena, tym lepiej.
+        </p>
+        <p>
+            <b>Maxi Cosi CabrioFix i-Size wypada bardzo dobrze. Ogólna ocena bezpieczeństwa fotelika wynosi 5, co jest
+                najwyższą możliwą oceną.</b> W crashtestach brano pod uwagę m.in. stabilność mocowania fotelika w
+            samochodzie podczas wypadku, zarówno w czasie zderzenia czołowego, jak i bocznego. Dodatkowo, w testach ADAC
+            ocenie podlegają również inne czynniki związane z użytkowaniem fotelika, takie jak łatwość montażu i
+            obsługi, ergonomię oraz jakość wykorzystanych materiałów. Ważne są zatem wszystkie kwestie, począwszy od
+            bezpieczeństwa, przez wygodę malucha, aż po komfort korzystania (montażu) dla rodziców.
+        </p>
+
+        <h4>Wygoda i komfort dziecka w czasie podróży</h4>
+        <p>
+            Mimo że testy zderzeniowe są najbardziej miarodajne, bo warunki, w jakich są testowane foteliki dla dzieci są naprawdę trudne (zupełnie jak w przypadku prawdziwej kolizji), to dużą uwagę należy zwrócić także na wygodę dziecka. Bardzo ważną kwestią jest ergonomia fotelika, szczególnie w przypadku fotelików dla noworodków i niemowląt do 1 roku życia. Dzieci po urodzeniu nie potrafią jeszcze samodzielnie utrzymać głowy, ich kręgosłup i mięśnie są na tyle słabe, że maluch nie potrafi utrzymać odpowiedniej pozycji ciała. Właściwa ergonomia fotelika i niemowlęcej wkładki pozwalają na wygodną i komfortową podróż, zapewniając odpowiednie wsparcie małemu człowiekowi. To dzięki ergonomii zarówno głowa, jak i tułów czy miednica są ułożone prawidłowo. Dlaczego jest to tak ważne? Ponieważ nieprawidłowości na tym etapie mogą mieć wpływ na całe życie dziecka Dodatkowo, u noworodków, odpowiednie ułożenie ciała zapewnia maluchowi drożność dróg oddechowych.
+        </p>
+        <p>
+            Maxi Cosi CabrioFix i-Size to bezpieczny, bardzo dobrej jakości fotelik, który zapewni dziecku bezpieczeństwo i wygodną jazdę. Dużym atutem jest możliwość montażu zarówno przy pomocy pasów samochodowych, jak również kompatybilnej bazy ISOFIX. Dodatkową zaletą jest możliwość umieszczenia fotelika na dopasowanym stelażu wózka.
+        </p>
+        <p>
+            Jeśli masz pytania dotyczące fotelika lub szukasz specjalistów, którzy pomogą Ci prawidłowo zamontować fotelik dla Twojego dziecka - skontaktuj się z nami.
+        </p>
+
     </>)
 }
 
-const prodSample3: SampleItem ={
+const prodSample3: SampleItem = {
     id: 3,
     label: "Więcej tekstów",
     collapsed: true,
-    description:(<a href="https://www.medicover.pl/o-zdrowiu/superbakteria-new-delhi-w-polsce-jak-uniknac-zakazenia,6317,n,3707" target="_blank">
-        <h4>Superbakteria New Delhi w Polsce - jak uniknąć zakażenia?</h4>
-        <p>
-            Artykuł dla portalu medicover.pl
-        </p>
-    </a>)
+    description: (
+        <a href="https://www.medicover.pl/o-zdrowiu/superbakteria-new-delhi-w-polsce-jak-uniknac-zakazenia,6317,n,3707"
+           target="_blank">
+            <h4>Superbakteria New Delhi w Polsce - jak uniknąć zakażenia?</h4>
+            <p>
+                Artykuł dla portalu medicover.pl
+            </p>
+        </a>)
 }
 
