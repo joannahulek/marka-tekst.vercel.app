8bbd28c9f42e5e2b6b31e6b63296471151c565ce
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-09 19:31:22 +0200

    2nd priority elements hidden (main website, call to action button)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero(){
                 <div className="col-lg-6 mx-auto">
                     <p className="lead mb-4">Dbam o Twoją markę w sieci</p>
                     <div className="d-grid gap-2 d-sm-flex justify-content-end">
-                        <a href="#copywriting" type="button" className="btn btn-outline-primary btn-lg px-4 gap-3">Moja oferta</a>
+                        <a href="#copywriting" type="button" className="d-none btn btn-outline-primary btn-lg px-4 gap-3">Moja oferta</a>
                     </div>
                 </div>
         </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 export default function Menu(){
 
-    const [activeTab, setActiveTab] = useState('#marka');
+    const [activeTab, setActiveTab] = useState('#about');
 
     const handleSelect = (selectedTab: string) => {
         setActiveTab(selectedTab);
@@ -24,12 +24,12 @@ export default function Menu(){
                 </button>
                 <div className="collapse navbar-collapse" id="navbarToggler">
                     <ul className="nav nav-tabs navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#marka"
+                        <li className="nav-item visually-hidden">
+                            <a className="nav-link" href="#marka"
                                onClick={() => handleSelect('#marka')}>Strona Główna</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about"
+                            <a className="nav-link active" aria-current="page" href="#about"
                                onClick={() => handleSelect('#about')}>O mnie</a>
                         </li>
                         <li className="nav-item">
