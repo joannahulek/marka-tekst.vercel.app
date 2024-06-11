26628f44b0c34285c8198cd09fbbcfea8bd9f238
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-11 09:33:32 +0200

    Revert "links still aren't work properly...let's try again"
    This reverts commit 583b06f438d1ce17bbce13c608190399e4c99295.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero(){
                 <div className="col-lg-6 mx-auto">
                     <p className="lead mb-4">Dbam o Twoją markę w sieci</p>
                     <div className="d-grid gap-2 d-sm-flex justify-content-end">
-                        <a href="https://marka-tekst.vercel.app/#copywriting" target="_self" type="button" className="btn btn-outline-primary btn-lg px-4 gap-3">Moja oferta</a>
+                        <a href="https://marka-tekst.vercel.app/#copywriting" type="button" className="btn btn-outline-primary btn-lg px-4 gap-3">Moja oferta</a>
                     </div>
                 </div>
         </div>
