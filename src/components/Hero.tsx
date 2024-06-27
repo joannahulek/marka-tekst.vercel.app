115d547e3b131ee94536a56af8919c67b86e80d7
Joanna Hulek <joanna.hulek@gmail.com>
2024-06-27 12:42:31 +0200

    call to action button handled

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero(){
                     <h1 className="display-4 fw-bold lh-1 text-body-emphasis">MarKa Tekst</h1>
                     <p className="lead text-center">Dbam o Twoją markę w sieci</p>
                     <div className="d-grid gap-2 d-md-flex justify-content-end mb-4 mb-lg-3">
-                        <a href="https://marka-tekst.vercel.app/#copywriting"><button type="button" className="btn btn-outline-secondary btn-lg px-4">Moja oferta</button></a>
+                        <a href="https://marka-tekst.vercel.app/#copywriting" target="_blank"><button type="button" className="btn btn-outline-secondary btn-lg px-4">Moja oferta</button></a>
                     </div>
                 </div>
                 <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-sm">
